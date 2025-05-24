3c7437ec800419e1ddfd5355c398239457414f1e
Tanmay <tanmaysharma763@gmail.com>
2025-05-24 12:06:35 +0530

    vercel erros

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,14 +1,17 @@
 // app/(auth)/sign-in/page.tsx
 "use client";
 
+import Loader from "@/components/Loader";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function SignInPage() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
 
   return (
+    <Suspense fallback={<Loader/>}>
     <div className="min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow-md text-center">
         <h1 className="text-2xl mb-4">Sign In</h1>
@@ -20,5 +23,6 @@ export default function SignInPage() {
         </button>
       </div>
     </div>
+    </Suspense>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images:{
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
+  
   typescript: {
     ignoreBuildErrors: true,
   },
