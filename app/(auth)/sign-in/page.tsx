a12eae79880ccca06d1b1808bcf1c7320d6ea9d2
Tanmay <tanmaysharma763@gmail.com>
2025-05-24 12:13:19 +0530

    use params should be wrapped inside a suspence block

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,27 +2,15 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { signIn } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
+import LoginPage from "@/components/Login";
+
 import { Suspense } from "react";
 
 export default function SignInPage() {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/";
-
+ 
   return (
     <Suspense fallback={<Loader/>}>
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md text-center">
-        <h1 className="text-2xl mb-4">Sign In</h1>
-        <button
-          onClick={() => signIn("google", { callbackUrl })}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Continue with Google
-        </button>
-      </div>
-    </div>
+      <LoginPage  />
     </Suspense>
   );
 }
---
+++ components/Login.tsx
@@ -0,0 +1,27 @@
+// app/(auth)/sign-in/page.tsx
+"use client";
+
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+export default function LoginPage() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  return (
+    
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md text-center">
+        <h1 className="text-2xl mb-4">Sign In</h1>
+        <button
+          onClick={() => signIn("google", { callbackUrl })}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Continue with Google
+        </button>
+      </div>
+    </div>
+    
+  );
+}
